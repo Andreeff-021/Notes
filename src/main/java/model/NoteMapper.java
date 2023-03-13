0bb6f50cc5b60ec6ed81c6c582f265c67784d41a
Andreeff-021 <andreeff.vladimir@yandex.ru>
2023-03-13 18:01:56 +0300

    add # instead of "," in NoteMapper

--- src/main/java/model/NoteMapper.java
+++ src/main/java/model/NoteMapper.java
@@ -2,11 +2,11 @@ package model;
 
 public class NoteMapper implements Mapper{
     public String map(Note note) {
-        return String.format("%s,%s,%s", note.getId(), note.getHeading(), note.getNote());
+        return String.format("%s#%s#%s", note.getId(), note.getHeading(), note.getNote());
     }
 
     public Note map(String line) {
-        String[] lines = line.split(",");
+        String[] lines = line.split("#");
         return new Note(lines[0], lines[1], lines[2]);
     }
 }
